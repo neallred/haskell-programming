a6d5e846c14ea2ea30754ebfd32e052b79ae4ab4
neallred <neallred@gmail.com>
2019-03-22 22:12:50 -0500

    added caesar cipher functions :)

--- chapter-09-cipher.hs
+++ chapter-09-cipher.hs
@@ -1,5 +1,47 @@
 module Chapter9Cipher where
 
 import Data.Char
+
 -- ord
 -- chr
+minUppercase = ord 'A' :: Int
+
+maxUppercase = ord 'Z' :: Int
+
+minLowercase = ord 'a' :: Int
+
+maxLowercase = ord 'z' :: Int
+
+shiftLetter :: Int -> Char -> Char
+shiftLetter 0 letter = letter
+shiftLetter shiftAmount letter =
+  let letterInt = ord letter
+      isLower = elem letterInt [minLowercase .. maxLowercase]
+      isUpper = elem letterInt [minUppercase .. maxUppercase]
+      isUnshiftable = not isLower && not isUpper
+   in if isUnshiftable
+        then letter
+        else let effectiveMin =
+                   if isLower
+                     then minLowercase
+                     else minUppercase
+                 effectiveMax =
+                   if isLower
+                     then maxLowercase
+                     else maxUppercase
+              in chr $
+                 effectiveMin +
+                 mod
+                   (letterInt + shiftAmount - effectiveMin)
+                   (effectiveMax - (effectiveMin - 1))
+
+makeCaesar :: Int -> String -> String
+makeCaesar x = map (shiftLetter x)
+
+makeUncaesar :: Int -> String -> String
+makeUncaesar x = map (shiftLetter (negate x))
+
+biumvirate :: Int -> ((String -> String), (String -> String))
+biumvirate x = (makeCaesar (negate (abs x)), makeCaesar (abs x))
+
+(uncaesar13, caesar13) = biumvirate 13
