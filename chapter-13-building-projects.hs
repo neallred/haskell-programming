9da05371e8c55ea8e566fbfacf85ec7304335961
neallred <neallred@gmail.com>
2019-05-07 20:05:28 -0500

    add to chapter 13

--- chapter-13-building-projects.hs
+++ chapter-13-building-projects.hs
@@ -1,36 +1,45 @@
+module Chapter13 where
+
+import Control.Monad (forever)
+import Data.Char (toLower)
+import Data.List (intersperse, sort)
+import Data.Maybe (fromMaybe, isJust)
+import System.Exit (exitSuccess)
+import System.IO
+import System.Random (randomRIO)
+
 -- Intermission: Check your understanding
------------------------------------------
-
--- 1.
-
--- forever, when are imported from Control.Monad
-
--- 2.
-
--- Unqualified, entire imports are Data.Bits and Database.Blacktip.Types
-
--- 3.
-
--- Types relevant to the Database.Blacktip module
-
--- 4.
-
--- a. MV = Control.Concurrent.MVar , FPC = Filesystem.Path.CurrentOS , CC = Control.Concurrent
-
--- b. Filesystem
-
--- c. Control.Monad
-
--- Making hangman
------------------
-
--- See hangman/ folder
-
--- Making hangman more robust
------------------------------
--- See hangman/ folder
--- DONE:
---
--- TODO:
--- Exit based only on incorrect number of guesses.
--- Make allowed number of mistakes inversely proportional to length of word.
+{-
+import qualified Control.Concurrent as CC
+import qualified Control.Concurrent.MVar as MV
+import Control.Exception (mask, try)
+import Control.Monad (forever, when)
+import Data.Bits
+import Data.Bits.Bitwise (fromListBE)
+import qualified Data.ByteString.Char8 as B
+import Data.List.Split (chunksOf)
+import qualified Data.Locator as DL
+import qualified Data.Time.Clock.POSIX as PSX
+import Database.Blacktip.Types
+import qualified Filesystem as FS
+import qualified Filesystem.Path.CurrentOS as FPC
+import qualified Network.Info as NI
+import qualified Safe
+import System.IO.Unsafe (unsafePerformIO)
+--}
+-- Given this import list,
+-- 1. forever and when are imported from Control.Monad
+-- 2. Data.Bits and Database.Blacktip.Types are imported unqualified and in their entirety
+-- 3. Importing Database.Blacktip.Types all the types and data constructors, and maybe some smart constructors
+-- 4. a. CC is Control.Concurrent, MV is Control.Concurrent.MVar, FPC Filesystem.Path.CurrentOS
+-- 4. b. FS.writeFile means Filesystem.writeFile
+-- 4. c. forever comes from Control.Monad
+twoo :: IO Bool
+twoo = do
+  c <- getChar
+  c' <- getChar
+  return (c == c')
+-- Chapter Exercises
+-- Hangman game logic
+-- See hangman/src/Main.js
+-- Modifying code
--- hangman/src/Main.hs
+++ hangman/src/Main.hs
@@ -1,21 +1,21 @@
 module Main where
 
-import           Control.Monad (forever)
-import           Data.Char     (toLower)
-import           Data.List     (intersperse)
-import           Data.Maybe    (isJust)
-import           System.Exit   (exitSuccess)
-import           System.Random (randomRIO)
+import Control.Monad (forever, when)
+import Data.Char (toLower)
+import Data.List (intersperse, sort)
+import Data.Maybe (fromMaybe, isJust)
+import System.Exit (exitSuccess)
+import System.IO
+import System.Random (randomRIO)
 
 newtype WordList =
-    WordList [String]
-    deriving (Eq, Show)
+  WordList [String]
+  deriving (Eq, Show)
 
 allWords :: IO WordList
 allWords = do
-    dict <- readFile "data/dict.txt"
-    return $ WordList (lines dict)
-
+  dict <- readFile "data/dict.txt"
+  return $ WordList (lines dict)
 
 minWordLength :: Int
 minWordLength = 5
@@ -23,112 +23,182 @@ minWordLength = 5
 maxWordLength :: Int
 maxWordLength = 9
 
+isSensibleWord :: String -> Bool
+isSensibleWord = all (`elem` (['A' .. 'Z'] ++ ['a' .. 'z']))
+
 gameWords :: IO WordList
 gameWords = do
-    (WordList aw) <- allWords
-    return $ WordList (filter gameLength aw)
-    where gameLength w =
-              let l = length (w :: String)
-              in        l >= minWordLength
-                    &&  l < maxWordLength
+  WordList aw <- allWords
+  return $ WordList (filter isSensibleWord (filter gameLength aw))
+  where
+    gameLength w =
+      let l = length (w :: String)
+       in l >= minWordLength && l < maxWordLength
 
 randomWord :: WordList -> IO String
 randomWord (WordList wl) = do
-    randomIndex <- randomRIO (0, length wl)
-    return $ wl !! randomIndex
+  randomIndex <- randomRIO (0, length wl - 1)
+  return $ wl !! randomIndex
 
 randomWord' :: IO String
 randomWord' = gameWords >>= randomWord
 
 data Puzzle =
-    Puzzle String [Maybe Char] [Char]
-
-instance Show Puzzle where
-    show (Puzzle _ discovered guessed) =
-        (intersperse ' ' $
-         fmap renderPuzzleChar discovered)
-        ++ " Guessed so far: " ++ guessed
+  Puzzle String
+         [Maybe Char]
+         String
 
 renderPuzzleChar :: Maybe Char -> Char
-renderPuzzleChar (Just x) = x
-renderPuzzleChar (Nothing) = '_'
+renderPuzzleChar = fromMaybe '_'
+
+instance Show Puzzle where
+  show puzzle@(Puzzle _ discovered guessed) =
+    intersperse ' ' (fmap renderPuzzleChar discovered) ++
+    " Wrong letters: " ++ wrongLetters puzzle
 
 freshPuzzle :: String -> Puzzle
 freshPuzzle xs = Puzzle xs (map (const Nothing) xs) []
 
 charInWord :: Puzzle -> Char -> Bool
-charInWord (Puzzle xs _ _) x = elem x xs
+charInWord (Puzzle puzzle _ _) x = x `elem` puzzle
 
 alreadyGuessed :: Puzzle -> Char -> Bool
-alreadyGuessed (Puzzle _ _ xs) x = elem x xs
+alreadyGuessed (Puzzle _ _ guessed) x = x `elem` guessed
 
 fillInCharacter :: Puzzle -> Char -> Puzzle
-fillInCharacter (Puzzle word filledInSoFar s) c = Puzzle word newFilledInSoFar (c:s)
-    where zipper guessed wordChar guessChar =
-              if wordChar == guessed
-                 then Just wordChar
-                 else guessChar
-
-          newFilledInSoFar =
-              zipWith (zipper c)
-                  word filledInSoFar
+fillInCharacter (Puzzle word filledInSoFar s) c =
+  Puzzle word newFilledInSoFar (sort (c : s))
+  where
+    zipper guessed wordChar guessChar =
+      if wordChar == guessed
+        then Just wordChar
+        else guessChar
+    newFilledInSoFar = zipWith (zipper c) word filledInSoFar
 
 handleGuess :: Puzzle -> Char -> IO Puzzle
 handleGuess puzzle guess = do
-    putStrLn $ "Your guess was: " ++ [guess]
-    case (charInWord puzzle guess
-         , alreadyGuessed puzzle guess) of
-         (_, True) -> do
-             putStrLn "You already guessed that\
+  putStrLn $ "Your guess was: " ++ [guess]
+  case (charInWord puzzle guess, alreadyGuessed puzzle guess) of
+    (_, True) -> do
+      putStrLn
+        "You already guessed that\
                  \ character, pick \
-                 \ somethign else!"
-             return puzzle
-         (True, _) -> do
-             putStrLn "This character was in the\
+                 \ something else!"
+      return puzzle
+    (True, _) -> do
+      putStrLn
+        "This character was in the\
                 \ word, filling in the word\
                 \ accordingly"
-             return (fillInCharacter puzzle guess)
-
-         (False, _) -> do
-             putStrLn "This character wasn't in\
+      return (fillInCharacter puzzle guess)
+    (False, _) -> do
+      putStrLn
+        "This character wasn't in\
                 \ the word, try again."
-             return (fillInCharacter puzzle guess)
+      return (fillInCharacter puzzle guess)
+
+boundedInt :: Int -> Int -> Int -> Int
+boundedInt minimum maximum number
+  | number < minimum = minimum
+  | number > minimum = maximum
+  | otherwise = number
+
+zeroToZeven = boundedInt 0 7
+
+baseWidth = 14 :: Int
+
+halfWidth = baseWidth `div` 2
+
+halfLessOne = replicate halfWidth
+
+leftBase = halfLessOne '_' ++ "|"
+
+rightBase = "|" ++ halfLessOne '_'
+
+emptyPoleLeft = halfLessOne ' ' ++ "|"
+
+emptyPole = emptyPoleLeft ++ "|" ++ halfLessOne ' ' ++ "\n"
+
+baseFloor = leftBase ++ rightBase ++ "\n"
+
+base = emptyPole ++ emptyPole ++ emptyPole ++ baseFloor
+
+mast = emptyPoleLeft ++ "|---|\n"
+
+bodyHead :: String
+bodyHead = emptyPoleLeft ++ "|" ++ replicate (halfWidth - 4) ' ' ++ "0\n"
+
+neck = emptyPoleLeft ++ "|" ++ "   |\n"
+
+neckLeftArm = emptyPoleLeft ++ "|" ++ " --|\n"
+
+neckLeftArmRightArm = emptyPoleLeft ++ "|" ++ " --|--\n"
+
+trunk = emptyPoleLeft ++ "|" ++ "   |\n"
+
+leftFoot = emptyPoleLeft ++ "|" ++ "  /\n"
+
+leftFootRightFoot = emptyPoleLeft ++ "|" ++ "  / \\\n"
+
+headerFooter x = mast ++ x ++ base
+
+-- render :: Int -> String
+-- render x
+--   | x <= 0 = ""
+drawBody :: Int -> String
+drawBody x
+  | x <= 0 = headerFooter (concat $ replicate 4 emptyPole)
+  | x == 1 = headerFooter (bodyHead ++ concat (replicate 3 emptyPole))
+  | x == 2 = headerFooter (bodyHead ++ neck ++ concat (replicate 2 emptyPole))
+  | x == 3 =
+    headerFooter (bodyHead ++ neckLeftArm ++ concat (replicate 2 emptyPole))
+  | x == 4 =
+    headerFooter
+      (bodyHead ++ neckLeftArmRightArm ++ concat (replicate 2 emptyPole))
+  | x == 5 =
+    headerFooter (bodyHead ++ neckLeftArmRightArm ++ trunk ++ emptyPole)
+  | x == 6 = headerFooter (bodyHead ++ neckLeftArmRightArm ++ trunk ++ leftFoot)
+  | x >= 7 =
+    headerFooter (bodyHead ++ neckLeftArmRightArm ++ trunk ++ leftFootRightFoot)
+
+maxErrors = 7
+
+wrongLetters :: Puzzle -> String
+wrongLetters (Puzzle wordToGuess _ guessed) =
+  filter (`notElem` wordToGuess) guessed
+
+numberWrong :: Puzzle -> Int
+numberWrong = length . wrongLetters
 
 gameOver :: Puzzle -> IO ()
-gameOver (Puzzle wordToGuess _ guessed) =
-    if (length guessed) > 7 then
-        do putStrLn "You lose!"
-           putStrLn $
-             "The word was: " ++ wordToGuess
-           exitSuccess
-
-    else return ()
+gameOver puzzle@(Puzzle wordToGuess _ guessed) =
+  when (numberWrong puzzle > maxErrors) $ do
+    putStrLn "You lose!"
+    putStrLn $ "The word was: " ++ wordToGuess
+    exitSuccess
 
 gameWin :: Puzzle -> IO ()
-gameWin (Puzzle _ filledInSoFar _) =
-    if all isJust filledInSoFar then
-        do putStrLn "You win!"
-           exitSuccess
-    else return ()
+gameWin (Puzzle thingToGuess filledInSoFar _) =
+  when (all isJust filledInSoFar) $ do
+    putStrLn $ "You win! The word was " ++ thingToGuess
+    exitSuccess
 
 runGame :: Puzzle -> IO ()
-runGame puzzle = forever $ do
+runGame puzzle =
+  forever $ do
     gameOver puzzle
     gameWin puzzle
-    putStrLn $
-        "Current puzzle is: " ++ show puzzle
-    putStrLn "Guess a letter: "
+    putStrLn $ "Current puzzle is: " ++ show puzzle
+    putStrLn (drawBody (numberWrong puzzle))
+    putStr "Guess a letter: "
     guess <- getLine
     case guess of
-         [c] -> handleGuess puzzle c >>= runGame
-         _   ->
-             putStrLn "Your guess must\
-                \ be a single character"
+      [c] -> handleGuess puzzle (toLower c) >>= runGame
+      _ -> putStrLn "Your guess must be a single character"
 
 main :: IO ()
 main = do
-    word <- randomWord'
-    let puzzle =
-            freshPuzzle (fmap toLower word)
-    runGame puzzle
-
+  hSetBuffering stdout NoBuffering
+  word <- randomWord'
+  let puzzle = freshPuzzle (fmap toLower word)
+  runGame puzzle
