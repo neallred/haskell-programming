0d8838fa2872b95de712a450aed7cd5df7f4bab8
neallred <neallred@gmail.com>
2019-05-20 20:36:09 -0500

    Some chapter 15 exercises

--- chapter-15-monoid-semigroup.hs
+++ chapter-15-monoid-semigroup.hs
@@ -1,26 +1,69 @@
--- Monoid laws
---------------
+module Chapter15 where
 
--- left identity
--- mappend mempty x = x
+import Data.Monoid
+import Data.Semigroup
+import Test.QuickCheck
 
--- right identity
--- mappend x mempty = x
+-- Semigroup - bijective associative function
+-- Monoid - Semigroup with an identity
+data Booly a
+  = False'
+  | True'
+  deriving (Eq, Show)
 
--- associativity
--- mappend x (mappend y z) =
---   mappend (mappend x y) z
+instance Semigroup (Booly a) where
+  (<>) False' _ = False'
+  (<>) _ False' = False'
+  (<>) True' True' = True'
 
--- mconcat = foldr mappend mempty
+instance Monoid (Booly a) where
+  mempty = False'
 
-data Optional a =
-  Nada
+--  mappend = (<>)
+-- Excercise: Optional Monoid
+data Optional a
+  = Nada
   | Only a
   deriving (Eq, Show)
 
-instance Monoid a
-  => Monoid (Optional a) where
-mempty = Nada
-mappend x Nada = x
-mappend Nada (Only x) = Only x
-mappend (Only x) (Only y) = Only (Prelude.mappend x y)
+instance Semigroup a => Semigroup (Optional a) where
+  (<>) Nada (Only x) = Only x
+  (<>) (Only x) Nada = Only x
+  (<>) (Only x) (Only y) = Only (x <> y)
+
+instance (Monoid a, Semigroup a) => Monoid (Optional a) where
+  mempty = Nada
+
+type Verb = String
+
+type Adjective = String
+
+type Adverb = String
+
+type Noun = String
+
+type Exclamation = String
+
+madlibbin' :: Exclamation -> Adverb -> Noun -> Adjective -> String
+madlibbin' e adv noun adj =
+  e <> "! he said " <> adv <> " as he jumped into his car " <> noun <>
+  " and drove off with his " <>
+  adj <>
+  " wife."
+
+-- Exercise: madlibbinBetter'
+madlibbinBetter :: Exclamation -> Adverb -> Noun -> Adjective -> String
+madlibbinBetter e adv noun adj =
+  mconcat
+    [ e
+    , "! he said "
+    , adv
+    , " as he jumped into his car "
+    , noun
+    , " and drove off with his "
+    , adj
+    , " wife."
+    ]
+-- Chapter Exercises:
+-- Semigroup Exercises
+-- See chapter15/src/Main.hs
---
+++ chapter15/src/Main.hs
@@ -0,0 +1,206 @@
+module Main where
+
+import Control.Monad
+import Data.List.NonEmpty
+import Data.Monoid (Monoid, mappend, mempty)
+import Data.Semigroup (Semigroup, (<>))
+import Test.QuickCheck
+
+-- import Test.QuickCheck.Gen
+-- import Test.QuickCheck.Property
+main :: IO ()
+main = do
+  putStrLn "Testing monoidal properties:"
+  putStrLn "Monoid assodiativity for string:"
+  monoidTestStr
+  putStrLn "Monoid left identity:"
+  quickCheck (monoidLeftIdentity :: String -> Bool)
+  putStrLn "Monoid right identity:"
+  quickCheck (monoidRightIdentity :: String -> Bool)
+  let ma = monoidAssoc
+      mli = monoidLeftIdentity
+      mri = monoidRightIdentity
+  quickCheck (ma :: BullMappend)
+  quickCheck (mli :: Bull -> Bool)
+  quickCheck (mri :: Bull -> Bool)
+  putStrLn "Testing Monoid for Optional type:"
+  putStrLn "testing mappend:"
+  quickCheck
+    (monoidAssoc :: First' String -> First' String -> First' String -> Bool)
+  putStrLn "testing left identity:"
+  quickCheck (monoidLeftIdentity :: First' String -> Bool)
+  putStrLn "testing right identity:"
+  quickCheck (monoidRightIdentity :: First' String -> Bool)
+  semigroupExercises
+
+-- asc :: Eq a => (a -> a -> a) -> a -> a -> Bool
+-- asc (<>) a b c = a <> (b <> c) == (a <> b) <> c
+monoidAssoc :: (Eq m, Semigroup m, Monoid m) => m -> m -> m -> Bool
+monoidAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
+
+monoidTestStr = quickCheck (monoidAssoc :: String -> String -> String -> Bool)
+
+monoidTestStr2 =
+  verboseCheck (monoidAssoc :: String -> String -> String -> Bool)
+
+monoidLeftIdentity :: (Eq m, Semigroup m, Monoid m) => m -> Bool
+monoidLeftIdentity a = (mempty <> a) == a
+
+monoidRightIdentity :: (Eq m, Semigroup m, Monoid m) => m -> Bool
+monoidRightIdentity a = (a <> mempty) == a
+
+data Bull
+  = Fools
+  | Twoo
+  deriving (Eq, Show)
+
+instance Arbitrary Bull where
+  arbitrary = frequency [(1, return Fools), (1, return Twoo)]
+
+instance Semigroup Bull where
+  (<>) _ _ = Fools
+
+instance Monoid Bull where
+  mempty = Fools
+  mappend _ _ = Fools
+
+type BullMappend = Bull -> Bull -> Bull -> Bool
+
+-- Exercise: Maybe another Monoid
+data Optional a
+  = Nada
+  | Only a
+  deriving (Eq, Show)
+
+newtype First' a =
+  First'
+    { getFirst' :: Optional a
+    }
+  deriving (Eq, Show)
+
+-- (Nada Only) Nada
+-- Nada (Only Nada)
+-- Nada (Only Nada)
+-- Only (Nada Only)
+-- (Only Nada) Only
+instance Semigroup (First' a) where
+  (<>) (First' (Only x)) (First' (Only y)) = First' (Only x)
+  (<>) (First' (Only x)) (First' Nada) = First' (Only x)
+  (<>) (First' Nada) (First' (Only x)) = First' (Only x)
+  (<>) (First' Nada) (First' Nada) = First' Nada
+
+instance Monoid (First' a) where
+  mempty = First' Nada
+  mappend = (<>)
+
+instance (Arbitrary a) => Arbitrary (First' a) where
+  arbitrary =
+    frequency [(1, return (First' Nada)), (4, fmap (First' . Only) arbitrary)]
+
+-- Chapter exercises
+-- Semigroup exercises
+-- 1.
+data Trivial =
+  Trivial
+  deriving (Eq, Show)
+
+instance Semigroup Trivial where
+  (<>) _ _ = Trivial
+
+instance Arbitrary Trivial where
+  arbitrary = return Trivial
+
+semigroupAssoc :: (Eq m, Semigroup m) => m -> m -> m -> Bool
+semigroupAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
+
+-- 2.
+newtype Identity a =
+  Identity a
+  deriving (Eq, Show)
+
+instance Semigroup (Identity a) where
+  (<>) (Identity a) (Identity b) = Identity a
+
+instance Arbitrary a => Arbitrary (Identity a) where
+  arbitrary = fmap Identity arbitrary
+
+-- 3.
+data Two a b =
+  Two a b
+  deriving (Eq, Show)
+
+pairToTwo :: (a, b) -> Two a b
+pairToTwo (x, y) = Two x y
+
+instance Semigroup (Two a b) where
+  (<>) (Two x1 _) (Two _ y2) = Two x1 y2
+
+twoGen :: (Arbitrary a, Arbitrary b) => Gen (Two a b)
+twoGen = do
+  x <- arbitrary
+  y <- arbitrary
+  return (Two x y)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
+  arbitrary = twoGen
+
+-- 4.
+data Three a b c =
+  Three a b c
+  deriving (Eq, Show)
+
+instance Semigroup (Three a b c) where
+  (<>) (Three _ _ c1) (Three a2 b2 _) = Three a2 b2 c1
+
+threeGen :: (Arbitrary a, Arbitrary b, Arbitrary c) => Gen (Three a b c)
+threeGen = do
+  x <- arbitrary
+  y <- arbitrary
+  z <- arbitrary
+  return (Three x y z)
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c) =>
+         Arbitrary (Three a b c) where
+  arbitrary = threeGen
+
+-- 5.
+data Four a b c d =
+  Four a b c d
+  deriving (Eq, Show)
+
+instance Semigroup (Four a b c d) where
+  (<>) (Four _ _ c1 d1) (Four a2 b2 _ _) = Four a2 b2 c1 d1
+
+fourGen ::
+     (Arbitrary a, Arbitrary b, Arbitrary c, Arbitrary d) => Gen (Four a b c d)
+fourGen = do
+  w <- arbitrary
+  x <- arbitrary
+  y <- arbitrary
+  z <- arbitrary
+  return (Four w x y z)
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c, Arbitrary d) =>
+         Arbitrary (Four a b c d) where
+  arbitrary = fourGen
+
+type WowThatsLong
+   = Four String Int [[String]] (Char, Bool) -> Four String Int [[String]] ( Char
+                                                                           , Bool) -> Four String Int [[String]] ( Char
+                                                                                                                 , Bool) -> Bool
+
+semigroupExercises :: IO ()
+semigroupExercises = do
+  putStrLn "Checking Trival semigroup property:"
+  quickCheck (semigroupAssoc :: Trivial -> Trivial -> Trivial -> Bool)
+  putStrLn "Checking Identity a semigroup property:"
+  quickCheck
+    (semigroupAssoc :: Identity String -> Identity String -> Identity String -> Bool)
+  putStrLn "Checking Two a b semigroup property:"
+  quickCheck
+    (semigroupAssoc :: Two String Int -> Two String Int -> Two String Int -> Bool)
+  putStrLn "Checking Three a b c semigroup property:"
+  quickCheck
+    (semigroupAssoc :: Three String Int [[String]] -> Three String Int [[String]] -> Three String Int [[String]] -> Bool)
+  putStrLn "Checking Four a b c d semigroup property:"
+  quickCheck (semigroupAssoc :: WowThatsLong)
